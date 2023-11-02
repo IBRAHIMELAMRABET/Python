96b908e90f1fc2e11d6fc561c6576f12bddf9212
IBRAHIMELAMRABET <brahimlmrabet41@gmail.com>
2023-11-02 13:59:14 +0100

    adding

--- tp3/ex1.py
+++ tp3/ex1.py
@@ -1,8 +1,12 @@
 def somme(m, n):
     total = 0
-    for i in range(m, n + 1):
-        total += i
+    if m<n:
+        for i in range(m, n + 1):
+             total += i
+    else :
+        for i in range(n, m + 1):
+             total += i
     return total
 
 resultat = somme(5, 8)
-print("La somme est :", resultat)
+print("La somme est : ", resultat)
